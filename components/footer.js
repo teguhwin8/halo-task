36f6de088d2354ce50181b0d76d209bcc16db421
Teguh Widodo <teguhwin8@gmail.com>
2022-06-11 22:13:46 +0700

    breadcrumb

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
 function Footer() {
-  return (
-    <div className="text-center p-3 text-sm text-slate-400 whitespace-nowrap">Copyright © {new Date().getFullYear()}. Halo Task</div>
-  )
+	return (
+		<div className="text-center p-3 text-sm text-slate-400 whitespace-nowrap">
+			Copyright © {new Date().getFullYear()}. Halo Task
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
--- components/topbar.js
+++ components/topbar.js
@@ -1,26 +1,30 @@
-import Link from "next/link";
 import { FcBusinessman } from "react-icons/fc";
-import { FcNext } from "react-icons/fc";
+import Breadcrumbs from "nextjs-breadcrumbs";
 
 function TopBar() {
-  return (
-    <div className="flex text-sm items-center justify-between">
-        <div className="breadcrumb flex items-center">
-            <Link href="/">
-                <a>Lobby</a>
-            </Link>
-            <FcNext className="mx-2" />
-            <Link href="#">
-                <a className="text-sky-600">Lobby</a>
-            </Link>
-        </div>
-        <div className="relative">
-            <div className="rounded-full cursor-pointer bg-red-200 w-8 h-8 flex justify-center items-center">
-                <FcBusinessman className="w-6 h-6" />
-            </div>
-        </div>
-    </div>
-  )
+	return (
+		<>
+			<div className="flex text-sm items-center justify-between">
+				<Breadcrumbs
+					rootLabel="Home"
+					useDefaultStyle={true}
+					inactiveItemClassName="inactive"
+					activeItemClassName="text-sky-600"
+					listStyle={{ display: "flex", textTransform: "capitalize" }}
+					inactiveItemStyle={{
+						paddingRight: ".5rem",
+						marginRight: ".5rem",
+					}}
+				/>
+
+				<div className="relative">
+					<div className="rounded-full cursor-pointer bg-red-200 w-8 h-8 flex justify-center items-center">
+						<FcBusinessman className="w-6 h-6" />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default TopBar
+export default TopBar;
