9c1d2ab2942e17c6c7c4ba5ca13cccfb942aa519
Teguh Widodo <teguhwin8@gmail.com>
2022-06-15 10:53:36 +0700

    firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import NextNProgress from "nextjs-progressbar";
-
+import "../src/config/firebase.config.js";
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
